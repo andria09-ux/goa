ba03cc9750794068c8fe221e41eb8ac7ef242b00
andria09-ux <andriatsatsua@gmail.com>
2024-09-13 21:25:03 +0400

    first commit

---
+++ day1/classwork/clasworkday2.py
@@ -0,0 +1,6 @@
+name = "andria"
+player_speed = (1.5)
+surname = "tsatsua"
+print(name)
+print(surname)
+print(player_speed)
---
+++ day1/classwork/level3.py
@@ -0,0 +1,21 @@
+score = (10)
+health = (20)
+print(score, health)
+attacking_points = (8)
+defence_points = (5)
+print(attacking_points, defence_points)
+
+math_score = (8)
+english_score = (10)
+science_score = (9)
+chemistry_score = (10)
+history_score = (7)
+print(math_score, english_score, science_score, chemistry_score, history_score)
+
+name = "მე მქვია ანდრია"
+surname = "ჩემი გვარია ცაცუა"
+age = "და ვარ 14 წლის"
+print(name, surname, age)
+
+
+
