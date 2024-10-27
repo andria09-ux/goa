f00301618009195796a5b076618101215cc8cbb5
andria09-ux <andriatsatsua@gmail.com>
2024-10-27 15:24:11 +0400

    second commit

---
+++ level15/homework.py
@@ -0,0 +1,35 @@
+# 1
+num = int(input("enter a number:"))
+if num < 10:
+    print("pass")
+else:
+    print("not pass")
+# 2
+if num % 2 == 0:
+    print("pass")
+else:
+    print("not pass")
+# 3
+if num > 0:
+    print("pass")
+else:
+    print("not pass")
+# 4
+num1 = int(input("enter the 1st number:"))
+num2 = int(input("enter the 2nd number:"))
+if num1 == num2:
+    print("pass")
+else:
+    print("not pass")
+# 5
+num_returned = int(input("enter a number:"))
+if num_returned > 100 and num_returned % 2 == 0:
+    print("pass")
+else:
+    print("not pass")
+# 6
+num_sequel = int(input("enter a number:"))
+if num_sequel % 5 == 0 or num_sequel % 10 == 0:
+    print("pass")
+else:
+    print("not pass")
