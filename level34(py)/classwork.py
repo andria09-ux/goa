0dfd945a6b8046be3483f4df4ff016362a925641
andria09-ux <andriatsatsua@gmail.com>
2025-01-01 23:41:50 +0400

    second commit

---
+++ level34(py)/classwork.py
@@ -0,0 +1,5 @@
+idk = ("avatar", "titanic", "Jurassic World", "Avengers: Endgame", "Avatar: The Way of Water", "Avengers: Infinity War")
+one, *rest = idk
+
+print(one, *rest[-1])
+
---
+++ level34(py)/homework.py
@@ -0,0 +1,18 @@
+# 1
+tp = (1,2,3,4,5)
+print(tp[0], tp[1])
+# 2
+numbers = (2, 56, 99, 22, 15, 23, 66, 11, 134, 23, 66, 91, 22, 2, 23)
+print(numbers.count(23))
+# 3
+aura = (10, 25, 5, 80, 70, 20)
+for point in aura:
+    if point > 10:
+        print(point)
+# 4
+
+# lists and tuples are very simmilar ,but list is mutable where as tuple is not.
+
+# 5
+
+# mutable is when a collection is able to be changes after its creation where as immutable is the opposite, its when a collection is non able to change after its creation.
