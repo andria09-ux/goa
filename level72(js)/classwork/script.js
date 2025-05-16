328f65f708f838d100f6a7cfe0190f519b346285
andria09-ux <andriatsatsua@gmail.com>
2025-05-16 23:46:14 +0400

    seconf commit

---
+++ level72(js)/classwork/script.js
@@ -0,0 +1,29 @@
+console.log("hello world")
+
+// let name;
+// let name = 2;
+name = "davit"
+name = "gocha"
+console.log(name);
+
+const array = "lomi"
+
+console.log(array)
+
+var age = 21
+var age = 24
+
+
+console.log("----------------------------------------------")
+
+
+const body = document.getElementById("body")
+console.log(body)
+
+function bgChange() {
+    body.style.background = "skyblue"
+}
+
+function dvChange() {
+    body.style.background = "red"
+}
